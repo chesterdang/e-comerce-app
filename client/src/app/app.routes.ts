db9df1e3d610087bed21db09bac4db9d18bbe571
Nguyen Van Danh <danhvannguyen000@gmail.com>
2024-10-10 14:20:09 +0700

    fix workflow

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -6,17 +6,7 @@ import {TestErrorComponent} from './features/test-error/test-error.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 import { ServerErrorComponent } from './shared/components/server-error/server-error.component';
 import { CartComponent } from './features/cart/cart.component';
-import { CheckoutComponent } from './features/checkout/checkout.component';
-import { LoginComponent } from './features/account/login/login.component';
-import { RegisterComponent } from './features/account/register/register.component';
-import { authGuard } from './core/guard/auth.guard';
-import { emptyGuard } from './core/guard/empty.guard';
-import { CheckoutSuccessComponent } from './features/checkout/checkout-success/checkout-success.component';
-import { OrderComponent } from './features/order/order.component';
-import { OrderDetailComponent } from './features/order/order-detail/order-detail.component';
-import { orderCompleteGuard } from './core/guard/order-complete.guard';
-import { AdminComponent } from './features/admin/admin.component';
-import { adminGuard } from './core/guard/admin.guard';
+
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'shop', component: ShopComponent},
